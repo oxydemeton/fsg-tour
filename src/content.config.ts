c7a7de05e4e1c60ace50fe058f22d75e382f76b7
M B <mablasteam@gmail.com>
2024-12-13 17:51:05 +0100

    added image collection

---
+++ src/content.config.ts
@@ -0,0 +1,21 @@
+import { defineCollection, reference, z } from 'astro:content';
+
+import { glob, file, } from 'astro/loaders';
+
+const rooms = defineCollection({
+    loader: glob({pattern: "**/room.json", base: "./src/data/rooms"}),
+    schema: ({image}) => z.object({
+        images: z.array(
+            z.object({
+                img: image(),
+                connection: z.optional(z.object({
+                    room: reference("rooms"),
+                    imageIndex: z.number()
+                }))
+            })
+        ),
+        circle: z.boolean(),
+    })
+});
+
+export const collections = {rooms}
