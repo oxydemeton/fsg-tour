f1602f11033c7575a95d1ab58dd4215040797786
M B <mablasteam@gmail.com>
2025-01-11 15:22:21 +0100

    update to new content system

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,21 +1,56 @@
 import { defineCollection, reference, z } from 'astro:content';
+import { file, } from 'astro/loaders';
+import { parseAll } from "@std/yaml";
 
-import { glob, file, } from 'astro/loaders';
 
-const rooms = defineCollection({
-    loader: glob({pattern: "**/*.json", base: "./src/data/rooms"}),
-    schema: ({image}) => z.object({
-        images: z.array(
-            z.object({
-                img: image(),
-                connection: z.optional(z.object({
-                    room: reference("rooms"),
-                    imageIndex: z.number()
-                }))
-            })
-        ),
-        circle: z.boolean(),
-    })
-});
+const room2 = defineCollection({
+    loader: file("./src/data/rooms.yaml", {parser: (txt)=>{
+        const fixedTxt = txt.split("\n").map(line=>{
+            if (line.trim().startsWith("- name:")) {
+                return "  " + line.trim()
+            } else if(line.trim().startsWith("verbindung:")) {
+                return "    "  + line.trim()
+            } else {
+                return line.trim()
+            }
+        }).join("\n")
+        const parsed = parseAll(fixedTxt, {onWarning: (err)=>console.error(err)}) as Record<string, unknown>[]
+        
+        const withId = parsed.map((raw, index)=>{
+            return {
+                ...raw,
+                slug: raw.nummer,
+                index,
+                bilder: raw.bilder as {name: string, verbindung?: string}[]
+            }
+        })
+        let imageCounter = 0
 
-export const collections = {rooms}
+        const withImage = withId.map(room => {
+            return {
+                ...room,
+                bilder: room.bilder.map(b=>{
+                    return {
+                        ...b,
+                        image: `./images/${imageCounter++}.png`
+                    }
+                })
+            }
+        })
+
+        //console.log(withImage);
+        return withImage
+    }}),
+    schema: ({image})=> z.object({
+        name: z.string(),
+        slug: z.string(),
+        index: z.number(),
+        bilder: z.array(z.object({
+            name: z.string(),
+            image: image(),
+            verbindung: z.optional(z.string())
+        }))
+    }),
+})
+
+export const collections = {room2}
