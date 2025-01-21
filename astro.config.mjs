9887f908e5cc3730342bb08611ac28249e7a74e6
oxydemeton <mabla@mabla.name>
2025-01-21 11:40:48 +0000

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   compressHTML: true,
   site: "https://oxydemeton.github.io",
   base: "fsg-tour",
