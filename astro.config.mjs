667addfd54e494837e691607403cfc88c65ffba6
M B <mablasteam@gmail.com>
2025-02-01 21:18:51 +0100

    upgraded to tailwind 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwind from '@astrojs/tailwind';
 
 import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap()],
+  integrations: [sitemap()],
   compressHTML: true,
   site: "https://oxydemeton.github.io",
   base: "fsg-tour",
-  cacheDir: "./buildCache"
+  cacheDir: "./buildCache",
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
