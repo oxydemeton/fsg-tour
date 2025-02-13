acfd2f1e29af9614b1efaaf40ee414fef36d115b
M B <mablasteam@gmail.com>
2025-02-13 20:05:54 +0100

    added api endpoint to get a list of all rooms

---
+++ src/pages/api/list.json.ts
@@ -0,0 +1,20 @@
+import type { APIRoute } from 'astro';
+import { getCollection } from "astro:content"
+const baseUrl = import.meta.env.BASE_URL
+
+
+const headers = new Headers({
+    "Content-Type": "application/json; charset=utf-8"
+})
+
+export const GET: APIRoute = async ({ url }) => {
+    const roomsCollected = await getCollection('rooms')
+    const rooms = roomsCollected.map(r=>({
+        name: r.data.name,
+        id: r.data.slug,
+        url: `${url.origin}${baseUrl}/${r.data.slug}/${r.data.bilder[0].name}`
+    }))    
+    
+    
+    return new Response(JSON.stringify(rooms), {status: 200, headers})
+}
